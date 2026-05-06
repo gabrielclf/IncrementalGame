d9ff7acd27899c5c18fd171de34a9cd90fa42ebf
Yur1W <165611276+Yur1W@users.noreply.github.com>
2026-05-06 14:53:40 -0300

    Added diferent sounds for first time buying upgrade

--- Assets/Scripts/Áudio/Upgrades AudioManager.cs
+++ Assets/Scripts/Áudio/Upgrades AudioManager.cs
@@ -5,11 +5,15 @@ using UnityEngine.EventSystems;
 
 public class UpgradesAudioManager : MonoBehaviour, IPointerEnterHandler
 {
+    [SerializeField] UpgradePontos upgradeData;
     [SerializeField] EventReference UpgradeSound;
+    [SerializeField] EventReference BuyUpgradeSound;
     [SerializeField] EventReference ButtonHoverSound;
+    private int upgradeLevel = 0;
     void Start()
     {
-        
+        //upgradeData = GetComponent<UpgradePontos>();
+        //UpgradePontos.QuantidadeUpgrades = 1f;
     }
     //Plays Sound when hovering over button
     public void OnPointerEnter(PointerEventData eventData)
@@ -18,8 +22,17 @@ public class UpgradesAudioManager : MonoBehaviour, IPointerEnterHandler
     }
 
     public void PlayUpgradeSound()
-    {
-        RuntimeManager.PlayOneShot(UpgradeSound);
+    {   
+        
+        if (upgradeLevel < 1)
+        {
+            RuntimeManager.PlayOneShot(BuyUpgradeSound);
+        }
+        else
+        {
+            RuntimeManager.PlayOneShot(UpgradeSound);
+        }
+        upgradeLevel++;
     }
 
     // Update is called once per frame

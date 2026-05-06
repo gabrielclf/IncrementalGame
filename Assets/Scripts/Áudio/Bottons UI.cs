339e63d5102f6466627553f23f8c86b634124e4a
Yur1W <165611276+Yur1W@users.noreply.github.com>
2026-05-06 09:44:56 -0300

    Added Check for button interativity

--- Assets/Scripts/Áudio/Bottons UI.cs
+++ Assets/Scripts/Áudio/Bottons UI.cs
@@ -2,19 +2,25 @@ using UnityEngine;
 using FMODUnity;
 using FMOD.Studio;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MenuButtons : MonoBehaviour, IPointerEnterHandler
 {
     [SerializeField] EventReference Button_Hover;
     [SerializeField] EventReference Button_Click;
+    private Button button;
     void Start()
     {
-        
+        button = GetComponent<Button>();
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
-        RuntimeManager.PlayOneShot(Button_Hover);
+        if (button.interactable)
+        {
+           RuntimeManager.PlayOneShot(Button_Hover);
+        } 
     }
+        
     public void PlayButtonClickSound()
     {
         RuntimeManager.PlayOneShot(Button_Click);

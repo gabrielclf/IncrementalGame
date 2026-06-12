a3d1f7427b1d1dd2bebb1d5a7de0c5d5867c180a
Gabriel_dcl_Fonseca <gabrieldclf@gmail.com>
2026-06-12 07:47:54 -0300

    Update 12/06/26
    Art Assets importados
    Implementação de assets de arte na UI (menos upgrades)
    Implementação de fontes
    Correção dos botões "Voltar" e "Resumir"

--- Assets/Scripts/ClickerManager.cs
+++ Assets/Scripts/ClickerManager.cs
@@ -125,11 +125,11 @@ public class ClickerManager : MonoBehaviour
         _upgradeCanvas.SetActive(true);
     }
 
-    public void ApertarBotaoVoltarJogo()
-    {
+    /*public void ApertarBotaoVoltarJogo()
+    { [DEPRECATED] - Operação feita dentro do unity editor
         _upgradeCanvas.SetActive(false);
         MainGameCanvas.SetActive(true);
-    }
+    }*/ 
 
     #endregion
 
